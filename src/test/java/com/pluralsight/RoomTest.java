6aa5c40d6f187528b4f4b2513d390db9c727ac96
michalhagos <mhayele3@gmail.com>
2026-05-10 22:25:06 -0500

    Added test to verify checkIn marks room as occupied

--- src/test/java/com/pluralsight/RoomTest.java
+++ src/test/java/com/pluralsight/RoomTest.java
@@ -1,7 +1,45 @@
 package com.pluralsight;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoomTest {
+
+    @Test
+    public void checkIn_should_markRoomOccupied() {
+        // arrange
+        Room room = new Room(2, 149.00, false, false);
+        // act
+        room.checkIn();
+        // assert
+        assertTrue(room.isOccupied());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
