7b93c50d1b06670e11bac883a0f52324776e3c0e
michalhagos <mhayele3@gmail.com>
2026-05-10 23:14:18 -0500

    Added test to verify punchOut calculates hours worked correctly

--- src/test/java/com/pluralsight/EmployeeTest.java
+++ src/test/java/com/pluralsight/EmployeeTest.java
@@ -1,7 +1,51 @@
 package com.pluralsight;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
+
+    @Test
+    public void punchOut_should_addHoursWorked() {
+        // arrange
+        Employee employee = new Employee(1, "Nobel", "Front Desk", 55.00, 0);
+        int expectedHours = 8;
+        // act
+        employee.punchIn(9);
+        employee.punchOut(17);
+        // assert
+        assertEquals(expectedHours, employee.getHoursWorked());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
